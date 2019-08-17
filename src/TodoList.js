7a4a8235aa2965235a5e1bc9f87ae4bf387c54e9
Toinne <anthony.meirlaen@gmail.com>
2019-08-17 17:24:01 +0200

    Reformat structure of item a bit

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,11 +3,11 @@ import React from 'react';
 // Renamed this from List item
 // Because this is not a single item it represents a list of todo's
 const TodoList = ({todos}, props) => {
-    console.log(todos);
     const todosList = todos.map(todo => {
         return (
-            <li key={todo.id} className="anything"><input type="checkbox"
-                                                          onClick={props.deleteItem}></input><span> {todo.task} {todo.deadline}</span>
+            <li key={todo.id} className="anything">
+                <input type="checkbox" onClick={props.deleteItem} />
+                <span> {todo.task} {todo.deadline}</span>
             </li>
         );
     });
